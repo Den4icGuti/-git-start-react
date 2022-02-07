ef4cbc3fdd4088fc82295421ce6aaacd0d768634
Den4icGuti <goos7412@gmail.com>
2022-02-07 14:35:38 +0200

    add config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import PaintingList from './components/paintingsList';
 import Section from './components/Section';
-import painting from './painting.json';
+// import painting from './painting.json';
 // import paintings from './painting.json';
 //====Создаем главный компонет в файле App.js====//
 export default function App() { 
